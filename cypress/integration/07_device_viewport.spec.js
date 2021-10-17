3a882b871881c606d6080ee3290302ffd459dab7
Farvater-max <farvater_max@mail.ru>
2021-10-18 00:25:56 +0300

    Check viewport

---
+++ cypress/integration/07_device_viewport.spec.js
@@ -0,0 +1,34 @@
+describe('Change device viewport', () => {
+    it('Viewport Iphone 6/7/8', () => {
+        cy.fixture('example.json').then(data => {
+            cy.log('Enter to main page')
+            cy.viewport(375, 667)
+            cy.visit(data.main_url)
+            cy.get('title').should('have.text', 'Тестирование')
+            cy.contains('.navbar-nav > :nth-child(1) > .nav-link').should('not.exist')
+            cy.contains('.form-inline > .btn').should('not.exist')  
+            cy.get('.navbar-toggler-icon')
+                .should('exist') 
+                .click() 
+
+            cy.get('.navbar-nav > :nth-child(1) > .nav-link').should('exist')
+            cy.get('.form-inline > .btn').should('exist')   
+        })
+    })
+    it('Viewport tablet', () => {
+        cy.fixture('example.json').then(data => {
+            cy.log('Enter to main page')
+            cy.viewport(768, 1024)
+            cy.visit(data.main_url)
+            cy.get('title').should('have.text', 'Тестирование')
+            cy.contains('.navbar-nav > :nth-child(1) > .nav-link').should('not.exist')
+            cy.contains('.form-inline > .btn').should('not.exist')  
+            cy.get('.navbar-toggler-icon')
+                .should('exist') 
+                .click() 
+
+            cy.get('.navbar-nav > :nth-child(1) > .nav-link').should('exist')
+            cy.get('.form-inline > .btn').should('exist')   
+        })
+    })
+});
